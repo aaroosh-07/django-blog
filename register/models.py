47d2c9d604f5a131ed0055568220f8948c1d3e13
chetak <ayushvidushi01@gmail.com>
2020-11-15 20:49:45 +0530

    description

--- register/models.py
+++ register/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User , on_delete=models.CASCADE)
+    description=models.CharField(max_length=300,null=True)
     image = models.ImageField(default="default.jpg",upload_to='Profile_pics')
 
     def __str__(self):
